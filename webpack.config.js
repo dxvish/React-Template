638c0342ca1f613172f2781ce03736909702b5c6
vishnu <dxvish@gmail.com>
2017-08-08 19:49:23 +0530

    Added stage-0 plugin for ES6 support

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
         //To specify the files we use test
         loader:'babel-loader',
         query :{
-          presets : ['react','es2015']
+          presets : ['react','es2015','stage-0']
         },
         test:/\.jsx?$/,
         //folders dont want to be parsed
